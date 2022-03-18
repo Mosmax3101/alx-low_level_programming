06a26dd32fd088363269388a1f5827873460cb6e
mosmax <abuduoluwabukola@gmail.com>
2022-03-18 05:15:46 -0700

    2-main.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description: printing alphabetical letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
